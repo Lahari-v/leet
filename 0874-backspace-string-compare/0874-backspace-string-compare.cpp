560082f2d236608eb141416468fd6b2f3aec7412
Lahari <159620907+Lahari-v@users.noreply.github.com>
2024-12-24 00:22:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<int> s1;
-        stack<int> s2;
-        for(char c : s) {
-            if (c != '#') {
-                s1.push(c);
-            } else if (!s1.empty()) {
-                s1.pop();
-            }
+        stack<char> s1, s2;
+
+        // Process the first string
+        for (char c : s) {
+            if (c == '#' && !s1.empty())
+                s1.pop();  // Remove the top if it's a backspace and the stack is not empty
+            else 
+                s1.push(c);  // Otherwise, add the character to the stack
         }
-        for(char c : t) {
-            if (c != '#') {
-                s2.push(c);
-            } else if (!s2.empty()) {
-                s2.pop();
-            }
+
+        // Process the second string
+        for (char c : t) {
+            if (c == '#' && !s2.empty())
+                s2.pop();  // Same as above, handle backspace
+            else 
+                s2.push(c);  // Otherwise, push the character onto the stack
         }
-        while(!s1.empty() && !s2.empty()) {
-            if(s1.top() != s2.top()) {
-                return false;
+
+        // Compare the two stacks element by element
+        while (!s1.empty() && !s2.empty()) {
+            if (s1.top() != s2.top()) {
+                return false;  // If any character doesn't match, return false
             }
             s1.pop();
             s2.pop();
         }
+
+        // After comparison, check if both stacks are empty
         return s1.empty() && s2.empty();
     }
 };
