bf0786d94327ce8e6f832c66404daee6f3396faf
Lahari <159620907+Lahari-v@users.noreply.github.com>
2024-12-27 15:58:13 +0530

    Time: 0 ms (100%), Space: 19.9 MB (42.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        vector<int> res; 
         int n = nums.size();
-        int cnt1=0, cnt2=0;
         int ele1 = INT_MIN, ele2 = INT_MIN;
-        vector<int> res;
-
+        int cnt1 = 0, cnt2 = 0;
         for(int i=0; i<n; i++) {
-            if(cnt1==0 && nums[i] != ele2) {
-                cnt1++;
+            if(cnt1 == 0 && nums[i] != ele2) {
                 ele1 = nums[i];
-            }
-            else if(cnt2==0 && nums[i] != ele1) {
-                cnt2++;
-                ele2 = nums[i];
-            }
-            else if(ele1 == nums[i]) {
                 cnt1++;
             }
-            else if(ele2 == nums[i]) {
+            else if(cnt2 == 0  && nums[i] != ele1) {
+                ele2 = nums[i];
                 cnt2++;
             }
+            else if(ele1 == nums[i]) 
+                cnt1++;
+            else if(ele2 == nums[i])
+                cnt2++;
             else {
                 cnt1--;
                 cnt2--;
             }
         }
-
         cnt1 = 0, cnt2 = 0;
         for(int i=0; i<n; i++) {
             if(ele1 == nums[i])
@@ -34,14 +30,12 @@ public:
             if(ele2 == nums[i])
                 cnt2++;
         }
-
-        int mini = int(n/3) + 1;
-        if(cnt1 >= mini) {
+        int min = int(n/3) + 1;
+        if(cnt1 >= min)
             res.push_back(ele1);
-        }
-        if(cnt2 >= mini) {
+        if(cnt2 >= min)
             res.push_back(ele2);
-        }
+        
         return res;
     }
 };
