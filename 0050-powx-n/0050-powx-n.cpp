918cd4b81a96808dd3721463a1eb7d7c8c3f31f4
Lahari <159620907+Lahari-v@users.noreply.github.com>
2025-01-18 16:41:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)
+        if(n == 0)
             return 1;
-        if(n<0)
-        {
-            n = abs(n);
+        int n1 = n; 
+        if(n1<0) {
+            n1 = -(long long)n;
             x = 1/x;
         }
-        if(n%2 == 0)
-        {
-            return myPow(x*x, n/2);
-        }
+        if(n1%2 == 0)
+            return myPow(x*x, n1/2);
         else
-        {
-            return x*myPow(x, n-1);
-        }
+            return x*myPow(x*x, n1/2);
     }
 };
